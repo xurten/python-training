f35e97104c21281db10d12c1aebe7c816e357298
Andrzej <xurten@gmail.com>
2022-04-06 22:41:29 +0200

    Create my_random.py

---
+++ my_random.py
@@ -0,0 +1,8 @@
+# Random order of elements
+from random import shuffle
+x = ['Keep', 'The', 'Blue', 'Flag', 'Flying', 'High']
+shuffle(x)
+print(x)
+my_list = [i for i in range(10)]
+shuffle(my_list)
+print(my_list)
