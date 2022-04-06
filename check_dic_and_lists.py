e0fe0d91076e6c1f24b3c8673540b7eaa2913d13
Andrzej <xurten@gmail.com>
2022-04-06 22:37:12 +0200

    Add reverse copy

--- check_dic_and_lists.py
+++ check_dic_and_lists.py
@@ -9,3 +9,8 @@ print(x[0:5:1])
 # Lambda function
 sum = lambda x, y: x + y
 print(sum(1000,2000))
+# Reverse copy of array and list
+import array as arr
+my_array = arr.array('i', [1,2,3,4,5])
+print(my_array[::-1])
+print([i for i in range(20)][::-1])
