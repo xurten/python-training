f037b360cd92a48c91e89be0a3aac49a58d6a564
Andrzej <xurten@gmail.com>
2022-12-09 09:17:15 +0100

    Create tip_27_use_list_comprehantion_instead_map_and_filter.py

---
+++ tips/tip_27_use_list_comprehantion_instead_map_and_filter.py
@@ -0,0 +1,24 @@
+# Tip 27 use list comprehantion instead of map() and filter()
+
+# examples for list
+numbers = [1, 2, 3, 4, 5, 6, 7]
+
+squares = [x ** 2 for x in numbers]
+print(squares)
+even_squares = [x ** 2 for x in numbers if x % 2 == 0]
+print(even_squares)
+# with map and filter
+alt = map(lambda x: x ** 2, filter(lambda x: x % 2 == 0, numbers))
+print(list(alt))
+
+# examples for dictionary
+even_squares_dict = {x: x ** 2 for x in numbers if x % 2 == 0}
+threes_cubed_set = {x ** 3 for x in numbers if x % 3 == 0}
+print(even_squares_dict)
+print(threes_cubed_set)
+
+# with map and filter
+alt_dict = dict(map(lambda x: (x, x ** 2), filter(lambda x: x % 2 == 0, numbers)))
+alt_set = set(map(lambda x: x ** 3, filter(lambda x: x % 3 == 0, numbers)))
+print(alt_dict)
+print(alt_set)
