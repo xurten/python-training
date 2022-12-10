f9639d376fcf8b70dd76636cf51bb56ce041ae02
Andrzej <xurten@gmail.com>
2022-12-10 11:17:38 +0100

    Create tip_25_for_readability_use_keyword_arguments.py

---
+++ tips/tip_25_for_readability_use_keyword_arguments.py
@@ -0,0 +1,87 @@
+# Tip 25 Use for readability keyword arguments
+
+# * on the arguments list means end of positional arguments and start with only keyword arguments
+# / end of positional arguments on the lists
+# between / and * we can pass positional arguments and keyword arguments
+
+def safe_division(number, divisor, ignore_overflow, ignore_zero_division):
+    try:
+        return number / divisor
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+
+def safe_division_b(number, divisor, ignore_overflow=False, ignore_zero_division=False):
+    try:
+        return number / divisor
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+
+def safe_division_c(number, divisor, *, ignore_overflow=False, ignore_zero_division=False):
+    try:
+        return number / divisor
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+
+def safe_division_d(number, divisor, /, *, ignore_overflow=False, ignore_zero_division=False):
+    try:
+        return number / divisor
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+
+def safe_division_e(numerator, denominator, /, ndigits=10, *, ignore_overflow=False, ignore_zero_division=False):
+    try:
+        fraction = numerator / denominator
+        return round(fraction, ndigits)
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+
+print(safe_division(22, 7, True, True))
+print(safe_division_b(22, 7))
+print(safe_division_c(22, 7))
+print(safe_division_d(22, 7))
+print(safe_division_e(22, 7))
