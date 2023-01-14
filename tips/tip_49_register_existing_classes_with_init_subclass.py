405cc7b4c41a8322ce60db775845ed3bfeb5fb52
Andrzej <xurten@gmail.com>
2023-01-14 19:00:49 +0100

    Update tip_49_register_existing_classes_with_init_subclass.py

--- tips/tip_49_register_existing_classes_with_init_subclass.py
+++ tips/tip_49_register_existing_classes_with_init_subclass.py
@@ -66,4 +66,55 @@ class BetterSerializable:
     def __repr__(self):
         name = self.__class__.__name__
         args_str = ', '.join(str(x) for x in self.args)
-        return f'{name}({args_str}'
+        return f'{name}({args_str})'
+
+
+registry = {}
+
+
+def register_class(target_class):
+    registry[target_class.__name__] = target_class
+
+
+def deserialize(data):
+    params = json.loads(data)
+    name = params['class']
+    target_class = registry[name]
+    return target_class(*params['args'])
+
+
+class EvenBetterPoint2D(BetterSerializable):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.x = x
+        self.y = y
+
+
+register_class(EvenBetterPoint2D)
+
+before = EvenBetterPoint2D(5, 3)
+print(before)
+data = before.serialize()
+print(data)
+after = deserialize(data)
+print(after)
+
+
+class BetterRegisteredSerializable(BetterSerializable):
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        register_class(cls)
+
+
+class Vector1D(BetterRegisteredSerializable):
+    def __init__(self, magnitude):
+        super().__init__(magnitude)
+        self.magnitude = magnitude
+
+
+before = Vector1D(6)
+print(before)
+data = before.serialize()
+print(data)
+after = deserialize(data)
+print(after)
