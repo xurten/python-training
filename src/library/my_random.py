9cd8a0b3088c307dc44f3705a27e0f1567215f71
Andrzej <xurten@gmail.com>
2023-01-14 19:26:56 +0100

    Clean up for class

--- src/library/my_random.py
+++ src/library/my_random.py
@@ -1,8 +1,15 @@
 # Random order of elements
 from random import shuffle
-x = ['Keep', 'The', 'Blue', 'Flag', 'Flying', 'High']
-shuffle(x)
-print(x)
-my_list = [i for i in range(10)]
-shuffle(my_list)
-print(my_list)
+
+
+def get_random_order_of_list(random_list):
+    shuffle(random_list)
+    print(random_list)
+    return random_list
+
+
+if __name__ == '__main__':
+    x = ['Keep', 'The', 'Blue', 'Flag', 'Flying', 'High']
+    get_random_order_of_list(x)
+    number_list = [i for i in range(10)]
+    get_random_order_of_list(number_list)
