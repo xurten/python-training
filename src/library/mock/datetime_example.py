732bbce11336988128d6a4cb78f884fc42ca6842
Andrzej <xurten@gmail.com>
2023-01-14 20:57:08 +0100

    Extract test to test section

--- src/library/mock/datetime_example.py
+++ src/library/mock/datetime_example.py
@@ -1,8 +1,5 @@
 import datetime
-from unittest import TestCase
 from unittest.mock import Mock
-
-from _pytest import unittest
 
 tuesday = datetime.datetime(year=2019, month=1, day=1)
 saturday = datetime.datetime(year=2019, month=1, day=5)
@@ -16,15 +13,5 @@ def is_weekday():
     return 0 <= today.weekday() < 5
 
 
-class TestCalendar(TestCase):
-    def test_tuesday(self):
-        datetime_mock.datetime.today.return_value = tuesday
-        assert is_weekday()
-
-    def test_saturday(self):
-        datetime_mock.datetime.today.return_value = saturday
-        assert not is_weekday()
-
-
 if __name__ == '__main__':
-    unittest.main()
+    print(is_weekday())
---
+++ tests/mock/test_datatime.py
@@ -0,0 +1,18 @@
+import unittest
+from unittest import TestCase
+
+from src.library.mock.datetime_example import tuesday, saturday, is_weekday, datetime_mock
+
+
+class TestCalendar(TestCase):
+    def test_tuesday(self):
+        datetime_mock.datetime.today.return_value = tuesday
+        assert is_weekday()
+
+    def test_saturday(self):
+        datetime_mock.datetime.today.return_value = saturday
+        assert not is_weekday()
+
+
+if __name__ == '__main__':
+    unittest.main()
