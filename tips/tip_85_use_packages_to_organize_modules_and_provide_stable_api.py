07fff1c122d9ed402b83d95fe53fde356d5444a5
Andrzej <xurten@gmail.com>
2023-01-15 21:55:44 +0100

    Update tip_85_use_packages_to_organize_modules_and_provide_stable_api.py

--- tips/tip_85_use_packages_to_organize_modules_and_provide_stable_api.py
+++ tips/tip_85_use_packages_to_organize_modules_and_provide_stable_api.py
@@ -1,4 +1,5 @@
 # Tip 85 use packages to organize modules and provide stable APIs
+# https://docs.python.org/3/tutorial/modules.html#tut-standardmodules
 # python packages has two primary goals:
 # - namespaces
 # - stable API
@@ -6,6 +7,8 @@
 # attribute __all__, all public methods you can import, if you specify it in file
 # you can select which methods, objects you can allow for import from other packages, if
 # no then all public elements are accessible
+# When importing the package, Python searches through the directories
+# on sys.path looking for the package subdirectory
 
 import io
 from io import open as new_open
