26e641e7c92c583dc83c0404e8714a84b20bb7df
Andrzej <xurten@gmail.com>
2023-01-17 19:01:39 +0100

    Create tip_90_consider_using_static_analysis_for_module.py

---
+++ tips/tip_90_consider_using_static_analysis_for_module.py
@@ -0,0 +1,5 @@
+# Tip 90 consider using static analysis for module
+# mypy https://mypy.readthedocs.io/en/stable/
+# pytype https://github.com/google/pytype
+# pyright https://github.com/microsoft/pyright
+# pyre https://pyre-check.org/docs/getting-started/
