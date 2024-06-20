9b48418acc1516422f47dc1fe32d0f9423b11813
Andrzej <xurten@gmail.com>
2024-06-20 16:44:18 +0200

    Add update for 3.8 and 3.10 python

---
+++ src/library/features_in_python_version/python_3.10.py
@@ -0,0 +1,41 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+"""
+Structural pattern matching
+"""
+
+
+def http_error(status):
+    match status:
+        case 400:
+            return "Bad request"
+        case 401 | 403 | 404:
+            return "Not allowed"
+        case 404:
+            return "Not found"
+        case 418:
+            return "I'm a teapot"
+        case _:
+            return "Something's wrong with the internet"
+
+
+def check_point(point):
+    # point is an (x, y) tuple
+    match point:
+        case (0, 0):
+            print("Origin")
+        case (0, y):
+            print(f"Y={y}")
+        case (x, 0):
+            print(f"X={x}")
+        case (x, y):
+            print(f"X={x}, Y={y}")
+        case _:
+            raise ValueError("Not a point")
+
+
+if __name__ == '__main__':
+    logger.error(http_error(401))
+    logger.error(check_point((100, 200)))
--- src/library/features_in_python_version/python_3_8.py
+++ src/library/features_in_python_version/python_3_8.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import date
 
 logger = logging.getLogger(__name__)
 
@@ -11,13 +12,28 @@ def the_walrus_operator():
 
 def positional_only_parameters():
     def f(a, b, /, c, d, *, e, f):
-        logger.error(a, b, c, d, e, f)
+        logger.error(f"{a=}, {b=}, {c=}, {d=}, {e=}, {f=}")
 
     f(10, 20, 30, d=40, e=50, f=60)
-    f(10, b=20, c=30, d=40, e=50, f=60)
-    f(10, 20, 30, 40, 50, f=60)
+    try:
+        f(10, b=20, c=30, d=40, e=50, f=60)
+    except Exception as ex:
+        logger.error(ex)
+    try:
+        f(10, 20, 30, 40, 50, f=60)
+    except Exception as ex:
+        logger.error(ex)
+
+
+def f_strings_self_documenting():
+    user = 'eric_idle'
+    member_since = date(1975, 7, 31)
+    logger.error(f'{user=} {member_since=}')
+    delta = date.today() - member_since
+    logger.error(f'{user=!s} {delta.days=:,d}')
 
 
 if __name__ == '__main__':
     the_walrus_operator()
     positional_only_parameters()
+    f_strings_self_documenting()
