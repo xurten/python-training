f943fbd15b1b19fc067cae8e18d9f426f942a327
Andrzej <xurten@gmail.com>
2025-01-21 16:53:15 +0100

    Do not init empty list as default

---
+++ src/library/weird_features/empty_list.py
@@ -0,0 +1,11 @@
+def append_to_list[T](value: T, my_list: list[T]= []) -> list[T]:
+    my_list.append(value)
+    return my_list
+
+
+first_result = append_to_list(1)
+print(first_result)
+
+
+second_result = append_to_list(2)
+print(second_result)
