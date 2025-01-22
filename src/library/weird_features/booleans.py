244b46299c11e98c53525be3e60ab6c230b67b58
Andrzej <xurten@gmail.com>
2025-01-22 13:40:27 +0100

    Create booleans.py

---
+++ src/library/weird_features/booleans.py
@@ -0,0 +1,11 @@
+def main() -> None:
+    if 1:
+        print("1 is True")
+
+    print(1 == True)
+    print(0 == False)
+    print(1 + True)
+    print(True + True)
+
+if __name__ == "__main__":
+    main()
